5e4292a7bd848e354d43da4f389c9942a140d0f8
MarSolor <msamtiagoortega@hotmail.com>
2024-08-11 22:01:13 -0600

    mejoras

--- cypress/e2e/CasosPrueba/CasoP03A.1.cy.js
+++ cypress/e2e/CasosPrueba/CasoP03A.1.cy.js
@@ -8,46 +8,37 @@ describe("Test Suite - Inicio de Sesión A", () => {
         // Iniciar el flujo de recuperación de contraseña
         iniciarRecuperacionContrasena("86147027");
 
-        // Ingresar el nuevo código de verificación y establecer nueva contraseña
-        establecerNuevaContrasena("0962", "Mario123.");
+        // Ingresar el código de verificación y establecer nueva contraseña
+        ingresarCodigoVerificacion("0908");
+        establecerNuevaContrasena("Mario123.");
 
         // Capturar la pantalla
         cy.screenshot();
     });
 
-    Cypress.on('uncaught:exception', (err, runnable) => {
+    Cypress.on('uncaught:exception', (err) => {
         // Retorna false para evitar que Cypress falle la prueba
         return false;
     });
 
     // Función para iniciar el flujo de recuperación de contraseña
     function iniciarRecuperacionContrasena(telefono) {
-        cy.get(':nth-child(4) > .text-sm')
-            .click()
-            .wait(100);
-        cy.get('.text-sm')
-            .type(telefono)
-            .wait(100);
-        cy.get(':nth-child(4) > .w-full')
-            .click()
-            .wait(100);
+        cy.get(':nth-child(4) > .text-sm').click();
+        cy.get('.text-sm').type(telefono);
+        cy.get(':nth-child(4) > .w-full').click();
+    }
+
+    // Función para ingresar el código de verificación recibido
+    function ingresarCodigoVerificacion(codigo) {
+        cy.get('.relative.mt-5 > #pass').type(codigo);
+        cy.get('.flex-row > .w-full').click(); // Hacer clic para verificar el código
     }
 
     // Función para establecer una nueva contraseña
-    function establecerNuevaContrasena(codigoVerificacion, nuevaContrasena) {
-        cy.get('.relative.mt-5 > #pass')
-            .type(nuevaContrasena)
-            .wait(100);
-        cy.get(':nth-child(2) > #pass')
-            .type(nuevaContrasena)
-            .wait(100);
-        cy.get('#pass1')
-            .type(nuevaContrasena)
-            .wait(100);
-        cy.get('.flex-row > .w-full')
-            .click()
-            .wait(1000);
+    function establecerNuevaContrasena(nuevaContrasena) {
+        cy.get(':nth-child(2) > #pass').type(nuevaContrasena);
+        cy.get('#pass1').type(nuevaContrasena);
+        cy.get('.flex-row > .w-full').click();
     }
 
 });
-
